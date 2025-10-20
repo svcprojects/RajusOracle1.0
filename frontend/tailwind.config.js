508f9b87849d05b5efb65306d1403544319d157c
lagnaoracle <lagnaoracle@gmail.com>
2025-10-20 14:36:12 +0200

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,1 +1,16 @@
-
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "lagna-gold": "#d4af37",
+        "lagna-blue": "#1e3a8a",
+      },
+    },
+  },
+  plugins: [],
+};
